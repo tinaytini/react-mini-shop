1e19877c0827f106df43351b029f4c97ca218e03
tinaytini <tinaytini@yahoo.com>
2022-04-12 13:09:15 +0200

    created product data and basket

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 console.log(data)
 
+
 function App() {
+  const [productList, setProduct] = React.useState(data)
+  const [basketList, setBasketList] = React.useState([])
+
+  const addToBasket = (i) => {
+    setBasketList([...basketList, productList[i]])
+  }
+
+
   return (
     <div className="App">
       <div className="product-list">
-        {data.map(product => (
-          <button key={product.id} className="product" >
+        {data.map((product, i) => (
+          <button onClick={() => addToBasket(i)} key={product.id} className="product" >
             <div>{product.title}</div>
             <div>{product.price}c</div>
-         </button>
+          </button>
         ))}
       </div>
       <div className="basket">
@@ -17,10 +26,12 @@ function App() {
           <span>0 c</span>
         </div>
         <div>
+        {basketList.map(item => (
           <button className="product" >
-            <div>Название</div>
-            <div>Цена c</div>
+            <div>{item.title}</div>
+            <div>{item.price}c</div>
           </button>
+        ))}
         </div>
       </div>
     </div>
