562a03fe56ec6aa455467061dd5fed7f8039d318
tinaytini <tinaytini@yahoo.com>
2022-04-12 14:53:08 +0200

    remove item after click

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-console.log(data)
-
 
 function App() {
   const [productList, setProduct] = React.useState(data)
   const [basketList, setBasketList] = React.useState([])
+  const [totalPrice, setTotalPrice] = React.useState(0)
+
 
   const addToBasket = (i) => {
     setBasketList([...basketList, productList[i]])
+    setTotalPrice(totalPrice + +productList[i].price)
+    
   }
 
-
+  const removeItem = (item, i) => {
+    setBasketList(basketList.splice(i, 1))
+    setTotalPrice(totalPrice - +item.price)
+    setBasketList([...basketList])
+  }
+  
+  
   return (
     <div className="App">
       <div className="product-list">
         {data.map((product, i) => (
-          <button onClick={() => addToBasket(i)} key={product.id} className="product" >
+          <button onClick={() => addToBasket(i)} key={i} className="product" >
             <div>{product.title}</div>
             <div>{product.price}c</div>
           </button>
@@ -23,11 +31,11 @@ function App() {
       <div className="basket">
         <div className="total">
           <span>Итого:</span>
-          <span>0 c</span>
+          <span>{totalPrice} c</span>
         </div>
         <div>
-        {basketList.map(item => (
-          <button className="product" >
+        {basketList.map((item, i) => (
+          <button onClick={() => (removeItem(item, i))} className="product" key={i}>
             <div>{item.title}</div>
             <div>{item.price}c</div>
           </button>
